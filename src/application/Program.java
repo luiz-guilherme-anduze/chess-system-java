31f5b009fc267aa8fe39415975e79bdbf1fbd3f3
Luiz Guilherme Anduze <l.g.anduze@gmail.com>
2022-03-31 15:50:46 -0300

    First Class: Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,11 +1,15 @@
 package application;
 
+import boardgame.Position;
+
 /**
  *
  * @author Luiz
  */
 public class Program {
     public static void main(String[] args){
-        System.out.println("Hello world!");
+        
+        Position pos = new Position(3, 5);
+        System.out.println(pos);
     }    
 }
